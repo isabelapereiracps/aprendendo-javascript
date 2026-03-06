5475318f0e2a4aa59e98242af883de98b4bc0058
Isabela Pereira <isabelapereira.cps@gmail.com>
2026-03-06 11:20:31 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//esse é um comentário no código, não será interpretado pelo JS
+
+//formas diferentes de criar variaveis:
+// 1 - variável global = somente o nome
+// 2 - var
+// 3 - let 
+// 3 - const
+
+//essa é uma variável que guarda o calor da minha idade atual
+let idade = 20
+
+//a data na qual eu faço aniversário
+const dataAniversario = '7 de Março'
+
+//imprimindo minha idade atual
+console.log('Minha idade atual é: ', idade)
+
+//imprimindo minha data de aniversário no console
+console.log('A data do meu aniversário é: ', dataAniversario)
+
+//idade depois que eu fiz aniversário
+idade = 21
+
+console.log('Minha idade depois do aniversário: ', idade)

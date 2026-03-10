1fb66a0704ce06d869895f001505df2d0f80d19d
Isabela Pereira <isabelapereira.cps@gmail.com>
2026-03-10 16:47:13 -0300

    fazendo o upload do curso até agora

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+console.log('Minha idade atual é:', idade)
+
+idade = 21
+//variavel nao pode ser referenciada antes da declaração
+//var pode
+//let, const não podem ser referenciadas antes da inicialização
+
+console.log('Minha idade atual depois de atribuir é:', idade)
---
+++ tipos.js
@@ -0,0 +1,33 @@
+let nome = 'Ana'
+let idade = 23
+let altura = 1.67
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof altura)
+
+let estaChovendo = false //false ou true
+
+console.log(typeof estaChovendo)
+
+let cidade
+
+console.log(typeof cidade)
+
+let aprendeuJS = null //ausência de valor
+
+console.log(typeof aprendeuJS)
+
+let simbolo = Symbol() //valor único e imutavel
+
+console.log(typeof simbolo)
+
+//possíveis tipos do JS:
+
+//string
+//number
+//number
+//boolean
+//undefined
+//object
+//symbol
